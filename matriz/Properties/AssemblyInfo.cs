499a10d539084a0076b9b16998ccfbedf3a11371
middagh01 <121726089+middagh01@users.noreply.github.com>
2024-12-09 22:56:56 -0400

    Update AssemblyInfo.cs

--- matriz/Properties/AssemblyInfo.cs
+++ matriz/Properties/AssemblyInfo.cs
@@ -34,3 +34,111 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+         public void pregunta2e()
+         {
+             int may = 0;
+             int elemay=0;
+             for (int c1 = 1; c1 <= c; c1++)
+             {
+                 may = 0;
+                for (int f1 = 1; f1 <= f; f1++)
+                 {
+                    int ele = x[f1, c1];
+                    int fr=frecuenciaCol(c1,ele);
+                    if (fr > may)
+                    {
+                        may = fr;
+                        elemay = ele;
+                    }
+                 }
+                x[f + 1, c1] = elemay;
+               x[f + 2, c1] = may;
+             }
+             f = f + 2;
+            
+         }
+
+        int frecuenciaCol(int c1, int ele)
+        {
+            int fr = 0;
+            for (int f1 = 1; f1 <= f; f1++)
+            {
+                if (x[f1, c1] == ele)
+                {
+                    fr++;
+                }
+            }
+            return fr;
+        }
+
+public void pregunta01()
+{
+    int s;
+    for (int j = 1; j <= this.c; j++)
+    {
+        s = 0;
+        for (int i = 1; i <= this.f; i++)
+        {
+            if (!this.pertenece(1, i - 1, j, x[i, j]))
+                s++;
+        }
+        x[f + 1, j] = s;
+    }
+    f++;
+    Ord_las_col_x_el_nro_de_eles_difsme();
+}
+
+public bool pertenece(int a, int b, int j, int g)
+{
+    int s = 0;
+    while (a <= b)
+    {
+        if (x[a, j] == g)
+        {
+            s++;
+        }
+        a++;
+    }
+    return s > 0;
+}
+
+public void Ord_las_col_x_el_nro_de_eles_difsme()
+{
+    for (var p = 1; p <= c - 1; p++)
+    {
+        for (var d = p + 1; d <= c; d++)
+        {
+            if (x[f, p] > x[f, d])
+                Intercambiar_col(d, p);
+        }
+    }
+}
+public void Intercambiar_col(int c1, int c2)
+{
+    for (var f1 = 1; f1 <= f; f1++)
+        Intercambiar(f1, c1, f1, c2);
+}
+
+public void Ordenar_col_asc()
+{
+    for (var c1 = 1; c1 <= c; c1++)
+
+    {
+        for (var f1 = 1; f1 <= f; f1++)
+        {
+            for (var f2 = f1 + 1; f2 <= f; f2++)
+            {
+                if (x[f1, c] < x[f2, c])
+                    Intercambiar(f1, c, f2, c);
+            }
+        }
+    }
+}
+
+public void Intercambiar(int f1, int c1, int f2, int c2)
+{
+    int aux = x[f1, c1];
+    x[f1, c1] = x[f2, c2];
+    x[f2, c2] = aux;
+}
